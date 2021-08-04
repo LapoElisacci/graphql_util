2d1d9dc733b4b7ffc54df923be5ca9536ae64170
Elisacci Lapo <lapoelisacci@gmail.com>
2021-08-04 16:09:14 +0200

    Fix HTTP token

--- lib/graphql_util/http.rb
+++ lib/graphql_util/http.rb
@@ -2,4 +2,20 @@
 
 require 'graphql/client/http'
 
-class GraphqlUtil::Http < GraphQL::Client::HTTP; end
+class GraphqlUtil::Http < GraphQL::Client::HTTP
+  attr_accessor :token
+  #
+  # Returns the GraphQL::Client::HTTP instance injecting the PulsarAuthUtil authentication Token
+  #
+  # @param [String] endpoint GraphQL API Endpoint
+  # @param [String] token GraphQL API Authentication Token
+  #
+  def initialize(endpoint, token)
+    @token = token
+    super(endpoint) do
+      def headers(context)
+        @token.present? ? { 'Authorization' => "Bearer #{@token}" } : {}
+      end
+    end
+  end
+end
