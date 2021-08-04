422e21543cf83429d974a53c6191a9716b22eab8
Elisacci Lapo <lapoelisacci@gmail.com>
2021-08-04 16:16:56 +0200

    Save

--- lib/graphql_util/client.rb
+++ lib/graphql_util/client.rb
@@ -2,4 +2,17 @@
 
 require 'graphql/client'
 
-class GraphqlUtil::Client < GraphQL::Client; end
+class GraphqlUtil::Client < GraphQL::Client
+  #
+  # Performs a GraphQL Query and handles the response
+  #
+  # @param [String] parsed_query GraphQL parsed query
+  # @param [Hash] variables GraphQL query params
+  # @param [Hash] context GraphQL query context
+  #
+  # @return [Monad] Succes(:data) / Failure(Exceptions::GraphqlError)
+  #
+  def query(parsed_query, variables: {}, context: {})
+    super(parsed_query, variables: variables, context: context)
+  end
+end
